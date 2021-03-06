38f8e859269841d049d2de6b4386954db8da0168
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-06 12:09:00 +0100

    add router links and component life-cycles

--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipies',
   templateUrl: './recipies.component.html',
   styleUrls: ['./recipies.component.css']
 })
-export class RecipiesComponent implements OnInit {
+export class RecipiesComponent implements OnInit, OnDestroy  {
 
   constructor() { }
 
+
   ngOnInit(): void {
+     console.log("Recipies component initialized");
+  }
+
+   ngOnDestroy(): void {
+    console.log("Recipies component destroyed");
   }
 
 }
